02b30152b020820affeb65af6ef87ddb7ce30199
John Goulah <jgoulah@etsy.com>
2010-07-06 15:15:44 +0000

    fix requiring of libraries

--- lib/twurl.rb
+++ lib/twurl.rb
@@ -5,8 +5,7 @@ require 'ostruct'
 require 'stringio'
 require 'yaml'
 
-library_files = Dir[File.join(File.dirname(__FILE__), "/twurl/**/*.rb")]
-require File.join(File.dirname(__FILE__), "twurl", "abstract_command_controller.rb")
+library_files = Dir[File.join(File.dirname(__FILE__), "/twurl/**/*.rb")].sort
 library_files.each do |file|
   require file
 end
